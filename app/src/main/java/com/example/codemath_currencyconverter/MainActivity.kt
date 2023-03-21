6f372791ec34f372d191e64914cfa5543b1c34b0
gabrielaliera <gliera2990@gmail.com>
2023-03-20 23:04:43 -0700

    Create calculate Cost function with binding values

--- app/src/main/java/com/example/codemath_currencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/codemath_currencyconverter/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.codemath_currencyconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.codemath_currencyconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    private val YEN_TO_USD:Double = 0.0076
+    private val yen get() = binding.userYen.text.toString().toDouble()
+    private val withdrawFee get() =binding.userWithdrawFee.text.toString().toDouble()
+
+    private val atmFee get() = binding.userATMFee.text.toString().toDouble()
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun calculateCost(): Double {
+        val convertToUSD = yen * atmFee * YEN_TO_USD
+        val total = convertToUSD + withdrawFee
+
+        return total
+    }
+
+    fun showResult(view:  View){
+        val result = calculateCost()
     }
 }
